dc37764a3520d36ea22a66aa94ac4205a291ff17
Natalie Weizenbaum <nweiz@google.com>
2019-09-03 16:48:35 -0700

    Fix the sha256

--- sass@1.23.0-module.beta.1.rb
+++ sass@1.23.0-module.beta.1.rb
@@ -5,7 +5,7 @@ class SassAT1230ModuleBeta1 < Sass
   homepage "https://sass-lang.com"
 
   url "https://github.com/sass/dart-sass/archive/1.23.0-module.beta.1.tar.gz"
-  sha256 "d24cde281e73c01a2d6104ce64df3e1ecd8595ef0f0b9f9927baaa69c97b2bfd"
+  sha256 "73d455eab1bee5ee031fcedf66f43f1e71c1561a54d159d517e49285560bfa56"
 
   depends_on "dart-lang/dart/dart" => :build
 end
