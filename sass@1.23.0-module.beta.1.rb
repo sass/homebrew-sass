9ec3a0fa50a0d5775d6122474735b42d9c223273
Natalie Weizenbaum <nweiz@google.com>
2019-09-03 13:22:25 -0700

    Add a beta release of the new module system

---
+++ sass@1.23.0-module.beta.1.rb
@@ -0,0 +1,12 @@
+require "yaml"
+require "sass"
+
+class SassATModuleBeta1 < Sass
+  desc "Stylesheet Preprocessor"
+  homepage "https://sass-lang.com"
+
+  url "https://github.com/sass/dart-sass/archive/1.23.0-module.beta.1.tar.gz"
+  sha256 "d24cde281e73c01a2d6104ce64df3e1ecd8595ef0f0b9f9927baaa69c97b2bfd"
+
+  depends_on "dart-lang/dart/dart" => :build
+end
