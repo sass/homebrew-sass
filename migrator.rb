c5b7b37031ea418b2ced56fe1b8518f3ebcd33ed
Natalie Weizenbaum <nweiz@google.com>
2021-10-05 15:53:09 -0700

    Fix brew audit failures

--- migrator.rb
+++ migrator.rb
@@ -1,5 +1,9 @@
+# typed: false
+# frozen_string_literal: true
+
 require "yaml"
 
+# A formula for the Sass migrator CLI.
 class Migrator < Formula
   desc "Sass Migration Tool"
   homepage "https://sass-lang.com/documentation/cli/migrator"
@@ -10,37 +14,16 @@ class Migrator < Formula
   depends_on "dart-lang/dart/dart" => :build
 
   def install
-    dart = Formula["dart-lang/dart/dart"].libexec/"bin"
-
-    pubspec = YAML.safe_load(File.read("pubspec.yaml"))
-    version = pubspec["version"]
-
     # Tell the pub server where these installations are coming from.
     ENV["PUB_ENVIRONMENT"] = "homebrew:sass_migrator"
 
-    system dart/"pub", "get"
+    system _dart/"dart", "pub", "get"
+    # Build a native-code executable on 64-bit systems only. 32-bit Dart
+    # doesn't support this.
     if Hardware::CPU.is_64_bit?
-      # Build a native-code executable on 64-bit systems only. 32-bit Dart
-      # doesn't support this.
-      system dart/"dart2native", "-Dversion=#{version}", "bin/sass_migrator.dart",
-             "-o", "sass-migrator"
-      bin.install "sass-migrator"
+      _install_native_executable
     else
-      system dart/"dart",
-             "--snapshot=sass_migrator.dart.app.snapshot",
-             "--snapshot-kind=app-jit",
-             "bin/sass_migrator.dart", "tool/app-snapshot-input.scss"
-      lib.install "sass_migrator.dart.app.snapshot"
-
-      # Copy the version of the Dart VM we used into our lib directory so that if
-      # the user upgrades their Dart VM version it doesn't break Sass's snapshot,
-      # which was compiled with an older version.
-      cp dart/"dart", lib
-
-      (bin/"sass-migrator").write <<~SH
-        #!/bin/sh
-        exec "#{lib}/dart" "-Dversion=#{version}" "#{lib}/sass_migrator.dart.app.snapshot" "$@"
-      SH
+      _install_script_snapshot
     end
     chmod 0555, "#{bin}/sass-migrator"
   end
@@ -49,5 +32,39 @@ class Migrator < Formula
     (testpath/"test.scss").write("a {b: abs(-1)}")
     assert_match "b: math.abs(-1);",
                  shell_output("#{bin}/sass-migrator -nv module test.scss 2>&1")
+  end
+
+  private
+
+  def _dart
+    @_dart ||= Formula["dart-lang/dart/dart"].libexec/"bin"
+  end
+
+  def _version
+    @_version ||= YAML.safe_load(File.read("pubspec.yaml"))["version"]
+  end
+
+  def _install_native_executable
+    system _dart/"dart", "compile", "exe", "-Dversion=#{_version}",
+           "bin/sass_migrator.dart", "-o", "sass-migrator"
+    bin.install "sass-migrator"
+  end
+
+  def _install_script_snapshot
+    system _dart/"dart", "compile", "jit-snapshot",
+           "-Dversion=#{_version}",
+           "-o", "sass_migrator.dart.app.snapshot",
+           "bin/sass_migrator.dart", "tool/app-snapshot-input.scss"
+    lib.install "sass_mirator.dart.app.snapshot"
+
+    # Copy the version of the Dart VM we used into our lib directory so that if
+    # the user upgrades their Dart VM version it doesn't break Sass's snapshot,
+    # which was compiled with an older version.
+    cp _dart/"dart", lib
+
+    (bin/"sass-migrator").write <<~SH
+      #!/bin/sh
+      exec "#{lib}/dart" "#{lib}/sass_migrator.dart.app.snapshot" "$@"
+    SH
   end
 end
--- sass.rb
+++ sass.rb
@@ -1,5 +1,9 @@
+# typed: false
+# frozen_string_literal: true
+
 require "yaml"
 
+# A formula for the Sass CLI.
 class Sass < Formula
   desc "Stylesheet Preprocessor"
   homepage "https://sass-lang.com"
@@ -10,37 +14,16 @@ class Sass < Formula
   depends_on "dart-lang/dart/dart" => :build
 
   def install
-    dart = Formula["dart-lang/dart/dart"].libexec/"bin"
-
-    pubspec = YAML.safe_load(File.read("pubspec.yaml"))
-    version = pubspec["version"]
-
     # Tell the pub server where these installations are coming from.
     ENV["PUB_ENVIRONMENT"] = "homebrew:sass"
 
-    system dart/"dart", "pub", "get"
+    system _dart/"dart", "pub", "get"
+    # Build a native-code executable on 64-bit systems only. 32-bit Dart
+    # doesn't support this.
     if Hardware::CPU.is_64_bit?
-      # Build a native-code executable on 64-bit systems only. 32-bit Dart
-      # doesn't support this.
-      system dart/"dart", "compile", "exe", "-Dversion=#{version}",
-             "bin/sass.dart", "-o", "sass"
-      bin.install "sass"
+      _install_native_executable
     else
-      system dart/"dart", "compile", "jit-snapshot",
-             "-Dversion=#{version}",
-             "-o", "sass.dart.app.snapshot",
-             "bin/sass.dart", "tool/app-snapshot-input.scss"
-      lib.install "sass.dart.app.snapshot"
-
-      # Copy the version of the Dart VM we used into our lib directory so that if
-      # the user upgrades their Dart VM version it doesn't break Sass's snapshot,
-      # which was compiled with an older version.
-      cp dart/"dart", lib
-
-      (bin/"sass").write <<~SH
-        #!/bin/sh
-        exec "#{lib}/dart" "#{lib}/sass.dart.app.snapshot" "$@"
-      SH
+      _install_script_snapshot
     end
     chmod 0555, "#{bin}/sass"
   end
@@ -48,5 +31,39 @@ class Sass < Formula
   test do
     (testpath/"test.scss").write(".class {property: 1 + 1}")
     assert_match "property: 2;", shell_output("#{bin}/sass test.scss 2>&1")
+  end
+
+  private
+
+  def _dart
+    @_dart ||= Formula["dart-lang/dart/dart"].libexec/"bin"
+  end
+
+  def _version
+    @_version ||= YAML.safe_load(File.read("pubspec.yaml"))["version"]
+  end
+
+  def _install_native_executable
+    system _dart/"dart", "compile", "exe", "-Dversion=#{_version}",
+           "bin/sass.dart", "-o", "sass"
+    bin.install "sass"
+  end
+
+  def _install_script_snapshot
+    system _dart/"dart", "compile", "jit-snapshot",
+           "-Dversion=#{_version}",
+           "-o", "sass.dart.app.snapshot",
+           "bin/sass.dart", "tool/app-snapshot-input.scss"
+    lib.install "sass.dart.app.snapshot"
+
+    # Copy the version of the Dart VM we used into our lib directory so that if
+    # the user upgrades their Dart VM version it doesn't break Sass's snapshot,
+    # which was compiled with an older version.
+    cp _dart/"dart", lib
+
+    (bin/"sass").write <<~SH
+      #!/bin/sh
+      exec "#{lib}/dart" "#{lib}/sass.dart.app.snapshot" "$@"
+    SH
   end
 end
